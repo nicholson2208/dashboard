85e68d5043e07e433672e45b979fa0c448efebf6
Matt Nicholson <matthew.n.nicholson1@gmail.com>
2018-06-25 22:56:01 -0500

    restructure and config to my machine

--- dashboard/blog.py
+++ dashboard/blog.py
@@ -3,8 +3,8 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from dashboard.auth import login_required
+from dashboard.db import get_db
 
 bp = Blueprint('blog', __name__)
 
